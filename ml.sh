180f4df7c35af6f2328e85dc20e11e7252ddae24
Dhyoga <dhyogap@gmail.com>
2024-06-19 17:51:35 +0700

    f

---
+++ ml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/c369d4ky1284/ml
+python3 get_data.py
+python3 model1_training_script.py
+python3 model1_inference_script.py
+python3 model2_training_script.py
+python3 model2_inference_script.py
